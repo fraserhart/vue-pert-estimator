b54497e2f7c1f620fae036fc1fee6c038cee5c4a
Fraser Hart <fraser@fraser-hart.co.uk>
2017-10-02 07:17:19 +0100

    initial commit

---
+++ src/components/Calculator.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <table>
+      <tr>
+        <th>Task</th>
+        <th type="number">Optimistic</th>
+        <th type="number">Usual</th>
+        <th type="number">Pessimistic</th>
+        <th>Estimate</th>
+        <th>1SD</th>
+      </tr>
+      <tr v-for="estimate in estimations">
+        <td>{{ estimate.taskName }}</td>
+        <td>{{ estimate.optimistic }}</td>
+        <td>{{ estimate.usual }}</td>
+        <td>{{ estimate.pessimistic }}</td>
+        <td>{{ estimate.estimate }}</td>
+        <td>{{ estimate.variance }}</td>
+      </tr>
+      <tr>
+        <td colspan="4"></td>
+        <td>{{ totalEstimate() }}</td>
+        <td>{{ totalSD() }}</td>
+      </tr>
+      <tr>
+        <td><input v-model="taskName" /></td>
+        <td><input v-model="optimistic" /></td>
+        <td><input v-model="usual" /></td>
+        <td><input v-model="pessimistic" /></td>
+        <td><button v-on:click="addTask">Save</button></td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'PERT Estimator',
+      optimistic: 0,
+      usual: 0,
+      pessimistic: 0,
+      taskName: '',
+      estimations: [],
+    };
+  },
+  methods: {
+    addTask() {
+      this.estimations.push({
+        taskName: this.taskName,
+        optimistic: this.optimistic,
+        usual: this.usual,
+        pessimistic: this.pessimistic,
+        estimate: this.calculateEstimate(),
+        variance: this.calculateVariance(),
+      });
+    },
+    calculateEstimate() {
+      return ((Number(this.optimistic) + Number(this.pessimistic) +
+            (4 * Number(this.usual))) / 6).toFixed(2);
+    },
+    calculateVariance() {
+      return ((this.pessimistic - this.optimistic) ** 2) / (6 ** 2);
+    },
+    totalEstimate() {
+      if (this.estimations.length < 1) {
+        return 0;
+      }
+      return this.estimations.reduce((total, e) => total + Number(e.estimate), 0);
+    },
+    totalSD() {
+      if (this.estimations.length < 1) {
+        return 0;
+      }
+      return this.estimations.reduce((total, e) => total + Number(e.variance), 0);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
